190e83eac192b1025e5a731eaf7cada7090260e3
CarloRomeo427 <carlo.romeo.alt427@gmail.com>
2026-03-02 17:42:24 +0100

    fix speq mod

--- scripts/zelda_1.sh
+++ scripts/zelda_1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 for seed in 0 42 1234; do
     for env in HalfCheetah-v5; do
-        for epochs in 10000 25000 50000 100000
-        python main.py --algo speq_o2o --env $env --seed $seed --epochs 300 --log-wandb --offline-epochs $epochs
+        for epochs in 10000 25000 50000 100000; do
+            python main.py --algo speq_o2o --env $env --seed $seed --epochs 300 --log-wandb --offline-epochs $epochs --use-offline-data --dataset-quality expert
+        done
     done
 done
